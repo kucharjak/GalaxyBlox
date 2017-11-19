9ab024aa00557bbf1ea97a63f1a970f33858defb
Jakub Kuchař <kuchycz@gmail.com>
2017-11-19 21:39:48 +0100

    Added new splash screen (edited original with CRT "filter") - like this design really much

--- GalaxyBlox/Resources/Resource.Designer.cs
+++ GalaxyBlox/Resources/Resource.Designer.cs
@@ -50,6 +50,9 @@ namespace GalaxyBlox
 			// aapt resource value: 0x7f020001
 			public const int Splash = 2130837505;
 			
+			// aapt resource value: 0x7f020002
+			public const int Splash_CRT = 2130837506;
+			
 			static Drawable()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
